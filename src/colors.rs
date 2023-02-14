7f4fa9199d7cba0f0a8618ed5a3e3a9997539f68
evelyn maude <emaude@wpi.edu>
2023-02-14 17:17:42 -0500

    initial scaffolding

---
+++ src/colors.rs
@@ -0,0 +1,18 @@
+use crate::egui::Color32;
+
+pub const nord0: Color32 = Color32::from_rgb(46, 52, 64);
+pub const nord1: Color32 = Color32::from_rgb(59, 66, 82);
+pub const nord2: Color32 = Color32::from_rgb(67, 76, 94);
+pub const nord3: Color32 = Color32::from_rgb(76, 86, 106);
+pub const nord4: Color32 = Color32::from_rgb(216, 222, 233);
+pub const nord5: Color32 = Color32::from_rgb(229, 233, 240);
+pub const nord6: Color32 = Color32::from_rgb(236, 239, 244);
+pub const nord7: Color32 = Color32::from_rgb(143, 188, 187);
+pub const nord8: Color32 = Color32::from_rgb(136, 192, 208);
+pub const nord9: Color32 = Color32::from_rgb(129, 161, 193);
+pub const nord10: Color32 = Color32::from_rgb(94, 129, 172);
+pub const nord11: Color32 = Color32::from_rgb(191, 97, 106);
+pub const nord12: Color32 = Color32::from_rgb(208, 135, 112);
+pub const nord13: Color32 = Color32::from_rgb(235, 203, 139);
+pub const nord14: Color32 = Color32::from_rgb(163, 190, 140);
+pub const nord15: Color32 = Color32::from_rgb(180, 142, 173);
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::egui;
+use git2::{Repository, StatusOptions};
+
+mod file;
+mod colors;
+
+use self::file::File;
+
+fn main() -> Result<(), eframe::Error> {
+    // Log to stdout (if you run with `RUST_LOG=debug`).
+    tracing_subscriber::fmt::init();
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "wpi fsae git helper",
+        options,
+        Box::new(|_cc| Box::new(GitHelper::default())),
+    )
+}
+
+struct GitHelper {
+    active_folder: String,
+    repo: Option<Repository>
+}
+
+impl Default for GitHelper {
+    fn default() -> Self {
+        Self {
+            active_folder: "/home/evelyn/scratch/gittest".to_string(),
+            repo: Some(Repository::open("/home/evelyn/scratch/gittest").unwrap())
+        }
+    }
+}
+
+impl eframe::App for GitHelper {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("wpi fsae git helper");
+            ui.horizontal(|ui| {
+                if ui.button("select folder").clicked() {
+                    match tinyfiledialogs::select_folder_dialog("hi", &self.active_folder) {
+                        Some(folder) => {
+                            self.active_folder = folder;
+                            match Repository::open(&self.active_folder) {
+                                Ok(repo) => self.repo = Some(repo),
+                                Err(e) => println!("error opening {} as git repo: {}", self.active_folder, e),
+                            }
+                        },
+                        None => (),
+                    }
+                };
+                ui.label(self.active_folder.clone())
+            });
+            match &self.repo {
+                Some(repo) => {
+                    for f in repo.statuses(Some(StatusOptions::new().include_untracked(true))).unwrap().iter() {
+                        ui.add(File::new(f.path().unwrap()));
+                    };
+                },
+                None => (),
+            };
+        });
+    }
+}
